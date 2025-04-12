c37d93f16c1512eea13b821f27ff62b92146c69e
Masataka Suzuki <masa.9729taka@gmail.com>
2025-04-12 11:06:46 +0900

    Create main.py

---
+++ dir1/main.py
@@ -0,0 +1,10 @@
+from hello import say_hello, say_hello_to_person
+
+def main():
+  person = "Taro"
+  print(say_hello())
+  print()
+  print(say_hello_to_person(person))
+
+if __name__ == "__main__":
+  main()
